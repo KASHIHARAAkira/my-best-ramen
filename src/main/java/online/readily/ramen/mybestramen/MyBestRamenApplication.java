2221438b1434ad1a4f52862e2276a65fde8fa2eb
KASHIHARAAkira <akira.kashihara@hotmail.com>
2022-08-18 14:20:59 +0900

    Initial commit

---
+++ src/main/java/online/readily/ramen/mybestramen/MyBestRamenApplication.java
@@ -0,0 +1,13 @@
+package online.readily.ramen.mybestramen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBestRamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBestRamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/readily/ramen/mybestramen/MyBestRamenApplicationTests.java
@@ -0,0 +1,13 @@
+package online.readily.ramen.mybestramen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBestRamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
