6d43c3ba51f82a3bf19c03f778a904d4159a7d89
KASHIHARAAkira <akira.kashihara@hotmail.com>
2022-08-18 16:02:17 +0900

    Viewのファイルを映るようにした、Closure Libraryはまだ

---
+++ src/main/java/online/readily/ramen/mybestramen/PageController.java
@@ -0,0 +1,28 @@
+package online.readily.ramen.mybestramen;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class PageController {
+    @RequestMapping(value = "/")
+    String index(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    String login() {
+        return "login";
+    }
+
+    @RequestMapping("/mypage")
+    String mypage(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "mypage";
+    }
+
+}
