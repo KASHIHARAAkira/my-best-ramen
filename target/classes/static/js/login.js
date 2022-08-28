dc551dfbd318698630ac96ec46c62af9bcc1315b
Akira Kashihara <akira.kashihara@hotmail.com>
2022-08-28 16:32:46 +0900

    ログイン画面の枠を作成

---
+++ target/classes/static/js/login.js
@@ -0,0 +1,6 @@
+function main() {
+    const headerElement = common.components.makeHeader(document.getElementById("main-element"));
+    const footerElement = common.components.makeFooter(document.getElementById("main-element"));
+}
+
+main();
